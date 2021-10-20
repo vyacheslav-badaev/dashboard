729aead57450230c2e2a2339c527889bc8ccd956
Vyacheslav <vyacheslav.badaev@devit.group>
2021-10-20 08:43:02 +0000

    removed outdated jest config

--- test/unit/jest.conf.js
+++ test/unit/jest.conf.js
@@ -15,7 +15,6 @@ module.exports = {
   },
   snapshotSerializers: ['<rootDir>/node_modules/jest-serializer-vue'],
   setupFiles: ['<rootDir>/test/unit/setup'],
-  mapCoverage: true,
   coverageDirectory: '<rootDir>/test/unit/coverage',
   collectCoverageFrom: [
     'src*.{js,vue}',
