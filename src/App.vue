fa802270fe44478c348908239564f8959cab579b
Vyacheslav <vyacheslav.badaev@devit.group>
2022-03-05 23:55:46 +0000

    added vuetify frontend framework and basic vue layout structure

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,129 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
-  </div>
+  <v-app>
+    <v-navigation-drawer
+      fixed
+      :clipped="$vuetify.breakpoint.lgAndUp"
+      app
+      v-model="drawer"
+    >
+      <v-list>
+        <template v-for="item in items">
+          <v-layout
+            row
+            v-if="item.heading"
+            align-center
+            :key="item.heading"
+          >
+            <v-flex xs6>
+              <v-subheader v-if="item.heading">
+                {{ item.heading }}
+              </v-subheader>
+            </v-flex>
+            <v-flex xs6 class="text-xs-center">
+              <a href="#!" class="body-2 black--text">EDIT</a>
+            </v-flex>
+          </v-layout>
+          <v-list-group
+            v-else-if="item.children"
+            v-model="item.model"
+            :key="item.text"
+            :prepend-icon="item.model ? item.icon : item['icon-alt']"
+            append-icon=""
+          >
+            <v-list-tile slot="activator">
+              <v-list-tile-content>
+                <v-list-tile-title>
+                  {{ item.text }}
+                </v-list-tile-title>
+              </v-list-tile-content>
+            </v-list-tile>
+            <v-list-tile
+              v-for="(child, i) in item.children"
+              :key="i"
+              @click=""
+            >
+              <v-list-tile-action v-if="child.icon">
+                <v-icon>{{ child.icon }}</v-icon>
+              </v-list-tile-action>
+              <v-list-tile-content>
+                <v-list-tile-title>
+                  {{ child.text }}
+                </v-list-tile-title>
+              </v-list-tile-content>
+            </v-list-tile>
+          </v-list-group>
+          <v-list-tile v-else @click="" :key="item.text">
+            <v-list-tile-action>
+              <v-icon>{{ item.icon }}</v-icon>
+            </v-list-tile-action>
+            <v-list-tile-content>
+              <v-list-tile-title>
+                {{ item.text }}
+              </v-list-tile-title>
+            </v-list-tile-content>
+          </v-list-tile>
+        </template>
+      </v-list>
+    </v-navigation-drawer>
+    <v-toolbar
+      color="red darken-3"
+      dark
+      app
+      :clipped-left="$vuetify.breakpoint.lgAndUp"
+      fixed
+    >
+      <v-toolbar-title style="width: 300px" class="ml-0 pl-3">
+        <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
+        <v-avatar size="32px" tile>
+          <img
+            src="https:            alt="Vuetify"
+          >
+        </v-avatar>
+        <span class="hidden-sm-and-down">{{title}}</span>
+      </v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-btn icon large>
+        <v-avatar size="32px" tile>
+          <img
+            src="https:            alt="Vuetify"
+          >
+        </v-avatar>
+      </v-btn>
+    </v-toolbar>
+    <v-content>
+      <v-container fluid>
+        <router-view>
+        </router-view>
+      </v-container>
+    </v-content>
+  </v-app>
 </template>
 <script>
 export default {
-  name: 'App'
+  name: 'App',
+  data () {
+    return {
+      title: 'Spartan Dashboard',
+      drawer: false,
+      items: [
+        { icon: 'dashboard', text: 'Dashboard' },
+        {
+          icon: 'keyboard_arrow_up',
+          'icon-alt': 'keyboard_arrow_down',
+          text: 'Users',
+          model: true,
+          children: [
+            { icon: 'people', text: 'Students' },
+            { icon: 'school', text: 'Teachers' },
+            { icon: 'room_service', text: 'Servicedesk Employees' },
+            { icon: 'supervisor_account', text: 'Admins' }
+          ]
+        },
+        { icon: 'settings', text: 'Settings' }
+      ]
+    }
+  }
 }
 </script>
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
