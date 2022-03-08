b8b24b5c5adb276092c5050951c2f22be8c9e898
Vyacheslav <vyacheslav.badaev@devit.group>
2022-03-08 09:57:41 +0000

    updated layout

--- src/App.vue
+++ src/App.vue
@@ -1,97 +1,32 @@
 <template>
   <v-app>
-    <v-navigation-drawer
-      fixed
-      :clipped="$vuetify.breakpoint.lgAndUp"
-      app
-      v-model="drawer"
-    >
+    <v-navigation-drawer app fixed :clipped="$vuetify.breakpoint.lgAndUp" v-model="drawer">
       <v-list>
-        <template v-for="item in items">
-          <v-layout
-            row
-            v-if="item.heading"
-            align-center
-            :key="item.heading"
-          >
-            <v-flex xs6>
-              <v-subheader v-if="item.heading">
-                {{ item.heading }}
-              </v-subheader>
-            </v-flex>
-            <v-flex xs6 class="text-xs-center">
-              <a href="#!" class="body-2 black--text">EDIT</a>
-            </v-flex>
-          </v-layout>
-          <v-list-group
-            v-else-if="item.children"
-            v-model="item.model"
-            :key="item.text"
-            :prepend-icon="item.model ? item.icon : item['icon-alt']"
-            append-icon=""
-          >
-            <v-list-tile slot="activator">
-              <v-list-tile-content>
-                <v-list-tile-title>
-                  {{ item.text }}
-                </v-list-tile-title>
-              </v-list-tile-content>
-            </v-list-tile>
-            <v-list-tile
-              v-for="(child, i) in item.children"
-              :key="i"
-              @click=""
-            >
-              <v-list-tile-action v-if="child.icon">
-                <v-icon>{{ child.icon }}</v-icon>
-              </v-list-tile-action>
-              <v-list-tile-content>
-                <v-list-tile-title>
-                  {{ child.text }}
-                </v-list-tile-title>
-              </v-list-tile-content>
-            </v-list-tile>
-          </v-list-group>
-          <v-list-tile v-else @click="" :key="item.text">
-            <v-list-tile-action>
-              <v-icon>{{ item.icon }}</v-icon>
-            </v-list-tile-action>
-            <v-list-tile-content>
-              <v-list-tile-title>
-                {{ item.text }}
-              </v-list-tile-title>
-            </v-list-tile-content>
-          </v-list-tile>
-        </template>
+        <v-list-tile v-for="item in items" :key="item.title" @click="item.link">
+          <v-list-tile-action>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            <v-list-tile-title>{{ item.text }}</v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
       </v-list>
     </v-navigation-drawer>
-    <v-toolbar
-      color="red darken-3"
-      dark
-      app
-      :clipped-left="$vuetify.breakpoint.lgAndUp"
-      fixed
-    >
-      <v-toolbar-title style="width: 300px" class="ml-0 pl-3">
-        <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
-        <v-avatar size="32px" tile>
-          <img
-            src="https:            alt="Vuetify"
-          >
-        </v-avatar>
-        <span class="hidden-sm-and-down">{{title}}</span>
+    <v-toolbar app fixed :clipped-left="$vuetify.breakpoint.lgAndUp">
+      <v-toolbar-side-icon @click.stop="drawer = !drawer" />
+      <v-avatar size="45px" tile>
+        <img src="./assets/logos/spear/spear-logo.png" alt="Vuetify">
+      </v-avatar>
+      <v-toolbar-title class="mr-5 align-center">
+        <span class="title">{{title}}</span>
       </v-toolbar-title>
-      <v-spacer></v-spacer>
-      <v-btn icon large>
-        <v-avatar size="32px" tile>
-          <img
-            src="https:            alt="Vuetify"
-          >
-        </v-avatar>
+      <v-spacer />
+      <v-btn icon>
+        <v-icon>account_circle</v-icon>
       </v-btn>
     </v-toolbar>
     <v-content>
-      <v-container fluid>
+      <v-container fluid grid-list-xl>
         <router-view>
         </router-view>
       </v-container>
@@ -104,22 +39,12 @@ export default {
   data () {
     return {
       title: 'Spartan Dashboard',
-      drawer: false,
+      drawer: true,
       items: [
-        { icon: 'dashboard', text: 'Dashboard' },
-        {
-          icon: 'keyboard_arrow_up',
-          'icon-alt': 'keyboard_arrow_down',
-          text: 'Users',
-          model: true,
-          children: [
-            { icon: 'people', text: 'Students' },
-            { icon: 'school', text: 'Teachers' },
-            { icon: 'room_service', text: 'Servicedesk Employees' },
-            { icon: 'supervisor_account', text: 'Admins' }
-          ]
-        },
-        { icon: 'settings', text: 'Settings' }
+        {icon: 'people', text: 'Students', link: 'todo'},
+        {icon: 'school', text: 'Teachers', link: 'todo'},
+        {icon: 'room_service', text: 'Servicedesk Employees', link: 'todo'},
+        {icon: 'supervisor_account', text: 'Admins', link: 'todo'}
       ]
     }
   }
--- src/router/index.js
+++ src/router/index.js
@@ -1,13 +1,13 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import Demo from '@/components/Demo'
 Vue.use(Router)
 export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'Demo',
+      component: Demo
     }
   ]
 })
