c2bb4e0a4948a490dbffed9eaca96f0c00dab840
Vyacheslav <vyacheslav.badaev@devit.group>
2022-03-12 17:43:31 +0000

    added sentry error tracking

--- src/main.js
+++ src/main.js
@@ -4,6 +4,16 @@ import store from './store'
 import router from './router'
 import Vuetify from 'vuetify'
 import 'vuetify/dist/vuetify.min.css'
+import Raven from 'raven-js'
+import RavenVue from 'raven-js/plugins/vue'
+if (process.env.NODE_ENV === 'dev') {
+  Raven.setShouldSendCallback(() => { return false })
+  Raven.isSetup = () => { return true }
+}
+Raven
+  .config('https:
+  .addPlugin(RavenVue, Vue)
+  .install()
 Vue.use(Vuetify)
 Vue.config.productionTip = false
 new Vue({
