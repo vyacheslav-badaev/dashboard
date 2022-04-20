7171b5c554e4853f2bdfca655319248db6de0b9d
Vyacheslav <vyacheslav.badaev@devit.group>
2022-04-20 19:27:38 +0000

    added navigation component mini

--- src/App.vue
+++ src/App.vue
@@ -1,64 +1,33 @@
 <template>
   <v-app>
-    <v-navigation-drawer app fixed :clipped="$vuetify.breakpoint.lgAndUp" v-model="drawer">
-      <v-list>
-        <v-list-tile v-for="item in items" :key="item.title" @click="item.link">
-          <v-list-tile-action>
-            <v-icon>{{ item.icon }}</v-icon>
-          </v-list-tile-action>
-          <v-list-tile-content>
-            <v-list-tile-title>{{ item.text }}</v-list-tile-title>
-          </v-list-tile-content>
-        </v-list-tile>
-      </v-list>
-      <v-footer app>
-        <div class="center">
-          {{demotext}}
-        </div>
-      </v-footer>
-    </v-navigation-drawer>
-    <v-toolbar app fixed :clipped-left="$vuetify.breakpoint.lgAndUp">
-      <v-toolbar-side-icon @click.stop="drawer = !drawer" />
-      <v-avatar size="45px" tile>
-        <img src="./assets/logos/spear/spear-logo.png" alt="Vuetify">
-      </v-avatar>
-      <v-toolbar-title class="mr-5 align-center">
-        <span class="title">{{title}}</span>
-      </v-toolbar-title>
-      <v-spacer />
-      <v-btn icon>
-        <v-icon>account_circle</v-icon>
-      </v-btn>
-    </v-toolbar>
+    <Navigation />
     <v-content>
       <v-container fluid grid-list-xl>
-        <router-view>
-        </router-view>
+        <router-view />
       </v-container>
+      <v-snackbar v-model="snackbar">
+        {{buildInfo}}
+      </v-snackbar>
     </v-content>
   </v-app>
 </template>
 <script>
+import Navigation from './components/layout/Navigation'
 export default {
   name: 'App',
+  components: {
+    Navigation
+  },
   data () {
     return {
       title: 'Spartan Dashboard',
-      drawer: true,
-      items: [
-        {icon: 'people', text: 'Students', link: 'todo'},
-        {icon: 'school', text: 'Teachers', link: 'todo'},
-        {icon: 'room_service', text: 'Servicedesk Employees', link: 'todo'},
-        {icon: 'supervisor_account', text: 'Admins', link: 'todo'}
-      ]
+      snackbar: true
     }
   },
   computed: {
-    demotext () {
-      return 'Build on ' + new Date(parseInt(process.env.BUILD_TIME_UNIX_TIMESTAMP)) + ' with commit hash ' + process.env.BUILD_TIME_COMMIT_HASH
+    buildInfo () {
+      return 'Build on ' + new Date(parseInt(process.env.BUILD_TIME_UNIX_TIMESTAMP)).toLocaleString() + ' from commit ' + process.env.BUILD_TIME_COMMIT_HASH.slice(0, 8)
     }
   }
 }
 </script>
-<style>
-</style>
---
+++ src/components/layout/Navigation.vue
@@ -0,0 +1,72 @@
+<template>
+  <v-navigation-drawer app permanent :mini-variant="mini" class="">
+    <v-toolbar class="transparent">
+      <v-list class="pa-0">
+        <v-list-tile avatar>
+          <v-list-tile-avatar size="48">
+            <img src="./../../assets/logos/spear/spear-logo.png" >
+          </v-list-tile-avatar>
+          <v-list-tile-content>
+            <v-list-tile-title>{{title}}</v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+      </v-list>
+    </v-toolbar>
+    <v-list class="pt-3">
+      <v-list-tile v-for="item in items" :key="item.title" @click="item.title">
+        <v-list-tile-action>
+          <v-icon>{{ item.icon }}</v-icon>
+        </v-list-tile-action>
+        <v-list-tile-content>
+          <v-list-tile-title>{{ item.title }}</v-list-tile-title>
+        </v-list-tile-content>
+      </v-list-tile>
+    </v-list>
+    <v-footer flat absolute height="auto" class="transparent">
+      <v-list class="pr-0 footer--absolute">
+        <v-list-tile>
+          <v-list-tile-action>
+            <v-btn icon @click.native.stop="mini = !mini">
+              <v-icon>{{mini ? 'chevron_right' : 'chevron_left'}}</v-icon>
+            </v-btn>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            <v-list-tile-title>Collapse sidebar</v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+        <v-divider/>
+        <v-list-tile avatar>
+          <v-list-tile-avatar>
+            <img src="https:          </v-list-tile-avatar>
+          <v-list-tile-content>
+            <v-list-tile-title>John Doe</v-list-tile-title>
+          </v-list-tile-content>
+          <v-list-tile-action>
+            <v-btn icon>
+              <v-icon>exit_to_app</v-icon>
+            </v-btn>
+          </v-list-tile-action>
+        </v-list-tile>
+      </v-list>
+    </v-footer>
+  </v-navigation-drawer>
+</template>
+<script>
+export default {
+  name: 'Navigation',
+  data () {
+    return {
+      mini: true,
+      title: 'Spartan Dashboard',
+      items: [
+        { title: 'Dashboard', icon: 'dashboard' },
+        { title: 'Calender', icon: 'view_day' },
+        { title: 'Students', icon: 'school' },
+        { title: 'Teachers', icon: 'supervisor_account' },
+        { title: 'Servicedesk employees', icon: 'ac_unit' },
+        { title: 'Admins', icon: 'verified_user' }
+      ]
+    }
+  }
+}
+</script>
